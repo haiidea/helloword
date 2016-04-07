550fc6753e7f10d0c2b19ed0a830793e73e2355e
明海 <minghai@hanyongqiangdeMacBook-Pro.local>
2016-04-07 23:21:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/minghai/helloword/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.minghai.helloword;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by minghai on 16/4/7.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
